674fd2d058f893b44180eded95a475fcb5c3d46d
daniel <dev.danielcw@gmail.com>
2023-08-29 18:58:07 -0500

    logging in process

--- pages/api/github-callback.js
+++ pages/api/github-callback.js
@@ -5,12 +5,18 @@ import axios from 'axios';
 export default async function handler(req, res) {
   const { code } = req.query;
 
+  console.log(code)
+  console.log(req, res)
   // Exchange the authorization code for an access token
   
   try {
     const response = await axios.post(
       'https://github.com/login/oauth/access_token',
-      {},
+      {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      },
       {
         headers: {
           Accept: 'application/json',
@@ -19,7 +25,8 @@ export default async function handler(req, res) {
     );
 
     const accessToken = response.data.access_token;
-
+    console.log(accessToken)
+    // logs in the terminal***
     // Store the access token securely on your server and then
     // redirect the user to a page in your app. in this case, the main dashboard
     res.redirect('/dashboard');    
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,6 +3,7 @@ const Dashboard = () => {
     return (
         <div className="flex flex-col justify-center border-8 h-screen">
             <h1>* Main content page *</h1>
+            <button className="border-2 p-2"> Log out </button>
         </div>
     )
 }
